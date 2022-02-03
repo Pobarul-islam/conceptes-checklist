6938a551bd088c3cf24d9782c2e96ef8e23418a6
Pobarulislam <pobarul.islam10@gmail.com>
2022-02-03 13:09:20 +0600

    basic problem solving question added

---
+++ checklist.js
@@ -0,0 +1,27 @@
+// 1. write 3 variables (number, string, boolean)
+
+// 2. 2 variables (let, const).
+
+// 3. Simple math operation() +, -, *, / , %
+
+// 4. Comparison ()
+
+// 5. tow conditions. case-1: fulfill both conditions. case-2: fulfill at least one codition.
+
+// 6. if-else
+
+// 7. while loop to display 7 to 19 all numbers. only display odd numbers between 7 to 19.
+
+
+// 8. declare an array. number of elements. 4th update or changes position element. add or remove elements. check whether specific value exists in the array
+
+// 9. use any for loop to display every element of an array
+
+
+// 10. you have an array of numbers. display only the numbers bigger than 80
+
+// 11. write a function that takes three numbers and returns the multiplication of the three numbers
+
+// 12. declare an object and change any properthy of that
+
+
---
+++ problem-solving-checklist.js
@@ -0,0 +1,19 @@
+// 1. conversion
+feetToInch
+
+// 2. conversion
+centimeterToMeter
+
+
+// 3. calculation
+pageRequirements
+
+// book1 has 100 pages
+// book2 has 100 pages
+// book3 has 100 pages
+
+// 4. friends
+bestFriend
+
+// 5. will stop the loop if the array has any negatiove number and returns all the positiove number before the negative number.
+bestFriend
